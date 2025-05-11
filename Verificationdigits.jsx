7aed7b669cfebd3e2b5bb179ccaf4ff7be79c8ef
StevieC87 <92942489+StevieC87@users.noreply.github.com>
2025-05-11 21:10:37 +0200

    Update Verificationdigits.jsx

--- Verificationdigits.jsx
+++ Verificationdigits.jsx
@@ -8,6 +8,7 @@ export default function VerificationDigits({ email }) {
   const router = useRouter();
   const inputRefs = [useRef(), useRef(), useRef(), useRef(), useRef(), useRef()];
 
+  const [getcurrentfield, setGetCurrentField] = useState(0);
   const [digit1, setDigit1] = useState('');
   const [digit2, setDigit2] = useState('');
   const [digit3, setDigit3] = useState('');
@@ -51,6 +52,7 @@ export default function VerificationDigits({ email }) {
     let pastedData = e.clipboardData.getData('text/plain');
 
     const { name } = e.target;
+
     console.log('Pasted:', pastedData);
     if (isNaN(pastedData)) {
       e.preventDefault();
@@ -60,6 +62,7 @@ export default function VerificationDigits({ email }) {
     let pasteddatalength = pastedData.length;
     //. THIS IS IF USER TRIES TO ADD ONE VALUE TO AN EMPTY FIELD
     if (pasteddatalength === 1) {
+      let value = pastedData;
       switch (name) {
         case "firstdigit":
           inputRefs[idx].current.value = value;
@@ -97,33 +100,148 @@ export default function VerificationDigits({ email }) {
     if (pasteddatalength > 1) {
       //alert('mulla');
       //take first six vlaues 
-      const values = pastedData.split('');
+      const values1 = pastedData.split('');
+
+      //keep only first 6
+      //get current field
+      //if im on 0 field , dont matter
+      //if im on fied 1 : do only NOT 0, but
+
+      // const values = values1.slice(0, 6)
+      let values = values1.slice(0, 6)
+      const lengthvalues = values.length;
+
+      //    let currentfield = getcurrentfield;
+      //   let slicewewant = 5 - currentfield;
+      //WE WANNA SLICE THE TOTALS MINUS THE CURRENT FIELD, IF IT EXCEEDS 
+      let totalfields = 6;
+      let currentfield = getcurrentfield;
+      let slicethis
+      console.log('currentfield', currentfield);
+      let lengthofpastedvalues = values.length;
+      console.log('lengthofpastedvalues', lengthofpastedvalues);
+      let slicewewant;
+      if (lengthofpastedvalues > totalfields) {
+        slicewewant = totalfields - currentfield;
+        console.log('slicewewant', slicewewant);
+        let slicethis = values.slice(0, slicewewant);
+        console.log(slicethis, 'slicethis');
+      }
+      else {
+        slicethis = values;
+      }
+
+
+      values = slicethis;
+      console.log(values, 'values111');
+
+
+      let maxidx = 5;
+      console.log(idx, 'idx')
+      let idx0
+      let idx1;
+      let idx2;
+      let idx3;
+      let idx4;
+      let idx5;
+      if (idx === 0) {
+        idx0 = 0
+        idx1 = 1
+        idx2 = 2
+        idx3 = 3
+        idx4 = 4
+        idx5 = 5
+
+      }
+      else if (idx === 1) {
+        idx0 = null;
+        idx1 = 1
+        idx2 = 2
+        idx3 = 3
+        idx4 = 4
+        idx5 = 5
+      }
+      else if (idx === 2) {
+        idx0 = null
+        idx1 = null;
+        idx2 = 2
+        idx3 = 3
+        idx4 = 4
+        idx5 = 5
+      }
+      else if (idx === 3) {
+        idx0 = null
+        idx1 = null;
+        idx2 = null;
+        idx3 = 3
+        idx4 = 4
+        idx5 = 5
+      }
+      else if (idx === 4) {
+        idx0 = null
+        idx1 = null;
+        idx2 = null;
+        idx3 = null;
+        idx4 = 4
+        idx5 = 5
+      }
+      else if (idx === 5) {
+        idx0 = null
+        idx1 = null;
+        idx2 = null;
+        idx3 = null;
+        idx4 = null;
+        idx5 = 5;
+      }
+      //IF CURRENT FIELD IS SAY THE THIRD 
+      //THEN ON THAT FIELD WE PUT THE FIRST VALUE
+      //WE PUT THE VALUE + POSITION
+      console.log(values[0], 'values[0]');
+      console.log(values[1], 'values[1]');
+      console.log(values[2], 'values[2]');
+      console.log(values[3], 'values[3]');
+      console.log(values[4], 'values[4]');
+
+      console.log(values[5], 'values[5]');
+
+      console.log('idxes', idx0, idx1, idx2, idx3, idx4, idx5);
+      //if eg however only 2, and we are in 3rd field (so 4th field)
       console.log(values, 'values');
+      //could do if else but a lot of repetition
+      if (idx0 !== null) {
+        setDigit1(values[0 - currentfield] || '');
+        console.log(values[0 - currentfield], 'values[0]');
+        inputRefs[idx0].current.value = values[0 - currentfield] || '';
+      }
+      if (idx1 !== null) {
+        setDigit2(values[1 - currentfield] || '');
 
-      setDigit1(values[0] || '');
-      inputRefs[idx].current.value = values[0] || '';
-
-      setDigit2(values[1] || '');
-      inputRefs[idx + 1].current.value = values[1] || '';
-
-      setDigit3(values[2] || '');
-      inputRefs[idx + 2].current.value = values[2] || '';
-
-      setDigit4(values[3] || '');
-      inputRefs[idx + 3].current.value = values[3] || '';
-
-      setDigit5(values[4] || '');
-      inputRefs[idx + 4].current.value = values[4] || '';
-
-      setDigit6(values[5] || '');
-      inputRefs[idx + 5].current.value = values[5] || '';
-
+        inputRefs[idx1].current.value = values[1 - currentfield] || '';
+      }
+      if (idx2 !== null) {
+        setDigit3(values[2 - currentfield] || '');
+        inputRefs[idx2].current.value = values[2 - currentfield] || '';
+      }
+      if (idx3 !== null) {
+        setDigit4(values[3 - currentfield] || '');
+        inputRefs[idx3].current.value = values[3 - currentfield] || '';
+      }
+      if (idx4 !== null) {
+        setDigit5(values[4 - currentfield] || '');
+        inputRefs[idx4].current.value = values[4 - currentfield] || '';
+      }
+      if (idx5 !== null) {
+        setDigit6(values[5 - currentfield] || '');
+        inputRefs[idx5].current.value = values[5 - currentfield] || '';
+      }
+      //alternative - pastes eveyrhting from beginning every time
+      // inputRefs[0].current.value = values[0] || '';
       if (values.length <= 5) {
         inputRefs[pastedData.length].current.focus();
       }
 
       else if (values.length >= 6) {
-        inputRefs[pastedData.length - 1].current.focus();
+        // inputRefs[pastedData.length - 1].current.focus();
       }
       // const trimmedValue = value.slice(1, 2);
       // console.log(trimmedValue, 'trimmedValue');
@@ -433,6 +551,9 @@ export default function VerificationDigits({ email }) {
 
   }
 
+  const onFocusfield = (idx) => {
+    setGetCurrentField(idx);
+  }
   //. ================================================
 
   // ================================================
@@ -465,41 +586,47 @@ export default function VerificationDigits({ email }) {
             <p className="pb-5">We have sent a 6 digit code to  {email}. Please enter it below to verify your account.</p>
 
             <div className="flex flex-row justify-center items-center">
-              <input className="verifinput" ref={inputRefs[0]} default="" type="text" min="1" max="9" id="firstdigit" name="firstdigit"
+              <input className="verifinput" ref={inputRefs[0]} default="" type="text" min="1" max="9" inputMode="numeric" pattern="[0-9]*" id="firstdigit" name="firstdigit"
                 //  onChange={(e) => onChangeinput(e, 0)}
                 onPaste={(e) => handlePaste(e, 0)}
                 //onKeyUp={(e) => handlePaste(e, 4, 'keyboardpaste')}
                 onKeyDown={(e) => Keyboardpress(e, 0)}
+                onFocus={() => onFocusfield(0)}
               />
-              <input className="verifinput" ref={inputRefs[1]} type="text" min="1" max="9" id="firstdigit" name="seconddigit"
+              <input className="verifinput" inputMode="numeric" pattern="[0-9]*" ref={inputRefs[1]} type="text" min="1" max="9" id="  firstdigit" name="seconddigit"
                 //   onChange={(e) => onChangeinput(e, 1)}
                 onKeyDown={(e) => Keyboardpress(e, 1)}
                 //onKeyUp={(e) => handlePaste(e, 4, 'keyboardpaste')}
                 onPaste={(e) => handlePaste(e, 1)}
+                onFocus={() => onFocusfield(1)}
               />
-              <input className="verifinput" ref={inputRefs[2]} type="text" min="1" max="9" id="firstdigit" name="thirddigit"
+              <input className="verifinput" inputMode="numeric" pattern="[0-9]*" ref={inputRefs[2]} type="text" min="1" max="9" id="firstdigit" name="thirddigit"
                 //  onChange={(e) => onChangeinput(e, 2)}
                 onKeyDown={(e) => Keyboardpress(e, 2)}
                 //onKeyUp={(e) => handlePaste(e, 4, 'keyboardpaste')}
                 onPaste={(e) => handlePaste(e, 2)}
+                onFocus={() => onFocusfield(2)}
               />
-              <input className="verifinput" ref={inputRefs[3]} type="text" min="1" max="9" id="firstdigit" name="fourthdigit"
+              <input className="verifinput" inputMode="numeric" pattern="[0-9]*" ref={inputRefs[3]} type="text" min="1" max="9" id="firstdigit" name="fourthdigit"
                 //  onChange={(e) => onChangeinput(e, 3)}
                 onKeyDown={(e) => Keyboardpress(e, 3)}
                 //onKeyUp={(e) => handlePaste(e, 4, 'keyboardpaste')}
                 onPaste={(e) => handlePaste(e, 3)}
+                onFocus={() => onFocusfield(3)}
               />
-              <input className="verifinput" ref={inputRefs[4]} type="text" min="1" max="9" id="firstdigit" name="fifthdigit"
+              <input className="verifinput" inputMode="numeric" pattern="[0-9]*" ref={inputRefs[4]} type="text" min="1" max="9" id="firstdigit" name="fifthdigit"
                 //  onChange={(e) => onChangeinput(e, 4)}
                 onKeyDown={(e) => Keyboardpress(e, 4)}
                 //onKeyUp={(e) => handlePaste(e, 4, 'keyboardpaste')}
                 onPaste={(e) => handlePaste(e, 4)}
+                onFocus={() => onFocusfield(4)}
               />
-              <input className="verifinput" ref={inputRefs[5]} type="text" min="1" max="9" id="firstdigit" name="sixthdigit"
+              <input className="verifinput" inputMode="numeric" pattern="[0-9]*" ref={inputRefs[5]} type="text" min="1" max="9" id="firstdigit" name="sixthdigit"
                 //  onChange={(e) => onChangeinput(e, 5)}
                 onKeyDown={(e) => Keyboardpress(e, 5)}
                 // onKeyUp={(e) => handlePaste(e, 4, 'keyboardpaste')}
                 onPaste={(e) => handlePaste(e, 5)}
+                onFocus={() => onFocusfield(5)}
               />
             </div>
 
